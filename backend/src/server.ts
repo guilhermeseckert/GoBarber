95c521a0b3e23b58655d2848af66f05701bd23fb
Guilherme Sonnemann Eckert <guilhermeeckert@Guilherme.local>
2020-08-07 01:40:28 -0700

    config cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,11 +1,12 @@
 import 'reflect-metadata';
 import express from 'express';
+import cors from 'cors';
 import routes from './routes';
 import uploadConfig from './config/upload';
 import './database';
 
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
