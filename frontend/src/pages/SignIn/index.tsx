650fb697da80586debc43877a83c1b2da8a54547
Guilherme Sonnemann Eckert <guilhermeeckert@Guilherme.local>
2020-08-07 19:20:20 -0700

    consult api and sigIn

--- frontend/src/pages/SignIn/index.tsx
+++ frontend/src/pages/SignIn/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
@@ -9,34 +9,50 @@ import logoImg from '../../assets/logo.svg';
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 import getValidationErros from '../../utils/getValidationErros';
+import { AuthContext } from '../../context/AuthContext';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
-      const schema = Yup.object().shape({
-        email: Yup.string()
-          .required('E-mail is required')
-          .email('E-mail need to be valid'),
-        password: Yup.string().required('Password is requeired'),
-      });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      const erros = getValidationErros(err);
-      formRef.current?.setErrors(erros);
-    }
-  }, []);
+  const { signIn } = useContext(AuthContext);
+
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          email: Yup.string()
+            .required('E-mail is required')
+            .email('E-mail need to be valid'),
+          password: Yup.string().required('Password is requeired'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const erros = getValidationErros(err);
+        formRef.current?.setErrors(erros);
+      }
+    },
+    [signIn],
+  );
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="GoBarber" />
 
         <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Login in</h1>
+          <h1>Login In</h1>
           <Input name="email" icon={FiMail} placeholder="E-mail" />
           <Input
             name="password"
@@ -44,7 +60,7 @@ const SignIn: React.FC = () => {
             type="password"
             placeholder="Password"
           />
-          <Button type="submit">Sign in</Button>
+          <Button type="submit">Sign In</Button>
           <a href="forgot">Forgot password?</a>
         </Form>
 
