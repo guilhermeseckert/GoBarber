a2eb10232219d2c723f0f1c27c8c4416ecadcae2
Guilherme Sonnemann Eckert <guilhermeeckert@Guilherme.local>
2020-08-20 21:45:02 -0700

    starting dashboardd

---
+++ backend/src/modules/appointments/services/ListProviderAppointmentsServices.ts
@@ -0,0 +1,39 @@
+import 'reflect-metadata';
+import { injectable, inject } from 'tsyringe';
+import Appointment from '../infra/typeorm/entities/Appointment';
+import iAppointmentsRepository from '../repositories/iAppointmentsRepository';
+
+interface IRequest {
+  provider_id: string;
+  day: number;
+  month: number;
+  year: number;
+}
+
+@injectable()
+class ListProviderAppointmentsServices {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: iAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+    day,
+  }: IRequest): Promise<Appointment[]> {
+    const appointments = await this.appointmentsRepository.FindAllInDayFromProvider(
+      {
+        provider_id,
+        year,
+        month,
+        day,
+      },
+    );
+
+    return appointments;
+  }
+}
+
+export default ListProviderAppointmentsServices;
