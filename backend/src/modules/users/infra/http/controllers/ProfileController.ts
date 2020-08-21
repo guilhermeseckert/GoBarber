55efaca40f5ed678db9144a202e236e3930ed1f8
Guilherme Sonnemann Eckert <guilhermeeckert@Guilherme.local>
2020-08-21 01:34:00 -0700

    add classtransformer

--- backend/src/modules/users/infra/http/controllers/ProfileController.ts
+++ backend/src/modules/users/infra/http/controllers/ProfileController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { container } from 'tsyringe';
 import UpdateProfileService from '@modules/users/services/UpdateProfileService';
 import ShowProfileService from '@modules/users/services/ShowProfileService';
+import { classToClass } from 'class-transformer';
 
 export default class ProfileController {
   public async show(request: Request, response: Response): Promise<Response> {
@@ -10,9 +11,8 @@ export default class ProfileController {
     const showProfile = container.resolve(ShowProfileService);
 
     const user = await showProfile.execute({ user_id });
-    delete user.password;
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
@@ -31,6 +31,6 @@ export default class ProfileController {
 
     delete user.password;
 
-    return response.json(user);
+    return response.json(classToClass(user));
   }
 }
